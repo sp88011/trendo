2b40257561ca7c7edf6cd008729f9e599420b653
Max Makarochkin <mms.post@gmail.com>
2013-01-19 15:10:49 +0400

    Update trendo.rb

--- trendo.rb
+++ trendo.rb
@@ -17,17 +17,14 @@ end
 
 url = "http://www.google.com/trends/fetchComponent?hl=en&q=#{word}&date=#{month}/#{year}+1m&cmpt=q&content=1&cid=TIMESERIES_GRAPH_0&export=5&w=500&h=330"
 
-  page_content = open(url)
-  raw_data_array = page_content.split("rows\":")[1].split("var htmlChart").first
-  split_raw = raw_data_array.split("],")
-  
-  split_raw.each do |sr|
-    source = sr.split("Date(")[1].split(",")
-    date = source[0]+'-'+(source[1].gsub(/\s/,"").to_i+1).to_s+'-'+source[2].gsub(/\s/,"").gsub(/\)\}/,"") #.gsub(/)},/,'')
-    num = source[5]
-    puts "#{date}, #{num}"
-    File.open("result.csv", 'a') {|f| f.puts("#{date}, #{num}\n") }
-  end    
-# rescue
-#   raise "Detected..."
-# end
+page_content = open(url)
+raw_data_array = page_content.split("rows\":")[1].split("var htmlChart").first
+split_raw = raw_data_array.split("],")
+split_raw.each do |sr|
+  source = sr.split("Date(")[1].split(",")
+  date = source[0]+'-'+(source[1].gsub(/\s/,"").to_i+1).to_s+'-'+source[2].gsub(/\s/,"").gsub(/\)\}/,"") #.gsub(/)},/,'')
+  num = source[5]
+  puts "#{date}, #{num}"
+  File.open("result.csv", 'a') {|f| f.puts("#{date}, #{num}\n") }
+end    
+
